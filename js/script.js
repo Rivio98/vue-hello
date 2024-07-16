6b37c229445587434c0c27e89873fc2d505bc22a
Rivio98 <rivaldogjoni1998@gmail.com>
2024-07-16 13:47:04 +0200

    [UPD] added img adn bonus doned

--- js/script.js
+++ js/script.js
@@ -5,8 +5,10 @@ const { createApp, ref } = Vue
 createApp({
     setup() {//dentro il vue vado a dichiarere l'informazione da stampare sul dom
         const message = ref('Hello world!')//creo il messaggio
+        const image = ref('img/4d47d7e49cd325f8a13014d4950203b3.jpg')//immagine
         return {
-            message
+            message,
+            image
         }
     }
 }).mount('#container')
