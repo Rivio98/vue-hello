d642cf9af09a3e0e2dad0352afb19a4e0c93d958
Rivio98 <rivaldogjoni1998@gmail.com>
2024-07-16 13:36:11 +0200

    [UPD] Add vue and create message to put in the dom

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 //dichiaro nello script il vue
 
 const { createApp, ref } = Vue
+
+createApp({
+    setup() {//dentro il vue vado a dichiarere l'informazione da stampare sul dom
+        const message = ref('Hello world!')//creo il messaggio
+        return {
+            message
+        }
+    }
+}).mount('#container')
